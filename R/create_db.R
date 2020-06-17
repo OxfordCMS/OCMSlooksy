7924c1e8b9d1a2f2486e0f7dec94b0b89a36cd4a
syen <syen@kgen1.kennedy.ox.uk.ac>
2020-06-17 15:01:36 +0100

    added function to create csvdb file from dataframe or from file

--- R/create_db.R
+++ R/create_db.R
@@ -12,14 +12,41 @@
 #'   Unclassified taxa should be represented with \code{NA}.
 #' @param overwrite logic. default FALSE. Allow database tables to overwrite
 #'   existing tables of the same name.
-#' @param outdir output directory
+#' @param outdir output directory.
+#' @param fromfile default FALSE. read count table and taxonomy tables from 
+#'   file (tab delimited or comma delimited)
 #' @import RSQLite
 #' @export
 #' @return sqlite database with merged_abundance_id (counts) and
 #'   merged_taxonomy (taxonomy) in a sqlite database file named 'csvdb'
 
-create_db <- function(counts, taxonomy, overwrite = FALSE, outdir) {
+create_db <- function(counts, taxonomy, overwrite = FALSE, outdir, 
+                      fromfile = FALSE) {
   
+  # from file
+  if(fromfile) {
+    tryCatch({
+      ext <- tools::file_ext(counts)
+      counts <- switch(ext,
+                       csv = vroom::vroom(counts, delim = ","),
+                       tsv = vroom::vroom(counts, delim = "\t")
+      )
+    }, error = function() {
+      stop("Could not read in counts file. Check that the correct path is specified. File must be a tab or comma delimited file.")
+    }
+    )
+    
+    tryCatch({
+      ext <- tools::file_ext(taxonomy)
+      taxonomy <- switch(ext,
+                         csv = vroom::vroom(taxonomy, delim = ","),
+                         tsv = vroom::vroom(taxonomy, delim = "\t")
+      )
+    }, error = function() {
+      stop("Could not read in taxonomy file. Check that the correct path is specified. File must be a tab or comma delimited file.")
+    }
+    )
+  }
   # check counts
   if(!is.data.frame(counts)) {
     stop("counts must be a dataframe.")
@@ -72,8 +99,8 @@ create_db <- function(counts, taxonomy, overwrite = FALSE, outdir) {
   
   tryCatch({
       con <- dbConnect(SQLite(), file.path(outdir, 'csvdb'))
-      dbWriteTable(con, 'merged_abundance_id', counts)
-      dbWriteTable(con, 'merged_taxonomy', taxonomy)
+      dbWriteTable(con, 'merged_abundance_id', counts, overwrite = overwrite)
+      dbWriteTable(con, 'merged_taxonomy', taxonomy, overwrite = overwrite)
       dbDisconnect(con)
     }, error = function(e) {
       stop("Cannot create connection to output directory")
